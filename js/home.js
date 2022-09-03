1c90e3bb6ce7f12b32877c0140fe316f6197fd68
shalil-j <shaliljaiswar462@gmail.com>
2022-09-03 13:59:05 +0530

    1

--- js/home.js
+++ js/home.js
@@ -1,3 +1,19 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
 
 const counters = document.querySelectorAll('.counter');
 for (let n of counters) {
